2627c4f01a0fe4c1fc4288b17c6f9a6f23dbdc7c
Richard <git.nordstrom@gmail.com>
2023-04-09 17:54:51 +0200

    bugfix 404 code mountpoint script

--- executables/find-dbfs-mount-points.sh
+++ executables/find-dbfs-mount-points.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 STRICT_MODE=false
 MANUAL_IGNORE_LIST=''
+# Mountpoints used in databricks training material
 DEFAULT_IGNORE_LIST='["/mnt/dbacademy-datasets", "/mnt/dbacademy-users", "/mnt/training"]'
 
 function usage() {
@@ -42,32 +43,54 @@ else
     IGNORE_MOUNTPOINTS=$DEFAULT_IGNORE_LIST
 fi
 
-PARSED_RESPONSE=$(
+HTTPS_RESPONSE=$(
     curl -X GET "${DATABRICKS_HOST}/api/2.0/dbfs/list" \
+        --write-out "\nRESPONSE_CODE=%{response_code}\n" \
         --silent \
         --data '{ "path": "/mnt" }' \
-        -H "Authorization: Bearer $DATABRICKS_TOKEN" \
-        | jq . \
-        | jq --arg IGNORE_MOUNTPOINTS "$IGNORE_MOUNTPOINTS" \
-            '.files 
-        | map(select( .path as $in | $IGNORE_MOUNTPOINTS | index($in) | not)) 
-        | map(select( .is_dir = true ))'
+        -H "Authorization: Bearer $DATABRICKS_TOKEN"
 )
 
-echo "$PARSED_RESPONSE"
+REPSONSE_CODE=$(sed -n -e 's/^RESPONSE_CODE=//p' <<<"$HTTPS_RESPONSE")
+RESPONSE_BODY=$(sed -e '/^RESPONSE_CODE=/d' <<<"$HTTPS_RESPONSE")
 
-NUM_MOUNTPOINTS=$(
-    jq '. | length' <<<"$PARSED_RESPONSE"
-)
-
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-
-if [[ $NUM_MOUNTPOINTS -gt 0 ]]; then
-    echo -e "${RED}ERROR Number of mount points found: $NUM_MOUNTPOINTS ${NC}"
-    exit 1
-else
-    echo -e "${GREEN}No mount points found ${NC}"
+# From official docs:
+# List the contents of a directory, or details of the file. 
+# If the file or directory does not exist, 
+# this call throws an exception with RESOURCE_DOES_NOT_EXIST.
+if [[ "$REPSONSE_CODE" = 404 ]] && [[ "$RESPONSE_BODY" == *"RESOURCE_DOES_NOT_EXIST"* ]]; then
+    jq .message <<<"$RESPONSE_BODY"
     exit 0
+fi
+
+if [[ "$REPSONSE_CODE" = 200 ]]; then
+    PARSED_RESPONSE=$(
+        jq <<<"$RESPONSE_BODY" \
+            | jq --arg IGNORE_MOUNTPOINTS "$IGNORE_MOUNTPOINTS" \
+                '.files 
+            | map(select( .path as $in | $IGNORE_MOUNTPOINTS | index($in) | not)) 
+            | map(select( .is_dir = true ))'
+    )
+
+    echo "$PARSED_RESPONSE"
+
+    NUM_MOUNTPOINTS=$(
+        jq '. | length' <<<"$PARSED_RESPONSE"
+    )
+    RED='\033[0;31m'
+    NC='\033[0m'
+    GREEN='\033[0;32m'
+
+    if [[ $NUM_MOUNTPOINTS -gt 0 ]]; then
+        echo -e "${RED}ERROR Number of mount points found: $NUM_MOUNTPOINTS ${NC}"
+        exit 1
+    else
+        echo -e "${GREEN}No disallowed mount points found ${NC}"
+        exit 0
+    fi
+
+else
+    echo "::::Error::::"
+    echo "$HTTPS_RESPONSE"
+    exit 1
 fi
